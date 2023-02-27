c990b5d112f8e00b9dba8d8f46d70b08a143671c
zoranzoki21 <zorandori4444@gmail.com>
2023-02-27 15:21:30 +0100

    Stop using jsonlint for testing + update devDependencies
    Bug: T220036
    Change-Id: Ib30cbc8a4f87b9495a13bc2a8bc96a6bf4ca5e6d

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,39 +1,33 @@
-/* eslint-env es6 */
-/* eslint strict:0 */
-
+/* eslint-env node, es6 */
 module.exports = function ( grunt ) {
 	const conf = grunt.file.readJSON( 'extension.json' );
+
 	grunt.loadNpmTasks( 'grunt-banana-checker' );
 	grunt.loadNpmTasks( 'grunt-eslint' );
-	grunt.loadNpmTasks( 'grunt-jsonlint' );
 	grunt.loadNpmTasks( 'grunt-stylelint' );
 
 	grunt.initConfig( {
+		banana: conf.MessagesDirs,
 		eslint: {
 			options: {
+				extensions: [ '.js', '.json' ],
 				cache: true
 			},
 			all: [
-				'*.js',
-				'modules/**/*.js'
+				'**/*.{js,json}',
+				'!node_modules/**',
+				'!vendor/**'
 			]
 		},
 		stylelint: {
 			all: [
-				'*.{css,less}',
-				'modules/**/*.{css,less}'
-			]
-		},
-		banana: conf.MessagesDirs,
-		jsonlint: {
-			all: [
-				'*.json',
-				'**/*.json',
-				'!node_modules/**'
+				'**/*.{css,less}',
+				'!node_modules/**',
+				'!vendor/**'
 			]
 		}
 	} );
 
-	grunt.registerTask( 'test', [ 'eslint', 'stylelint', 'jsonlint', 'banana' ] );
+	grunt.registerTask( 'test', [ 'eslint', 'banana', 'stylelint' ] );
 	grunt.registerTask( 'default', 'test' );
 };
